dd4b8ffd89dc469d837b8f9ebda283299936989a
Markus Weiss <maweiss@c4c3c4.42berlin.de>
2024-02-05 20:48:22 +0100

    added the basic file structure

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 19:32:04 by maweiss           #+#    #+#             */
+/*   Updated: 2024/02/05 19:35:14 by maweiss          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "get_next_line_utils.c"
+
+#endif
