9202ae57338ac97b971d3b9ef0dc80011f0ba900
Markus Weiss <maweiss@c4c6c1.42berlin.de>
2024-02-07 14:16:22 +0100

    started coding

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,134 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 19:31:20 by maweiss           #+#    #+#             */
-/*   Updated: 2024/02/07 12:14:15 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/02/07 14:15:24 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Implement a get next line function:
 
 #include "get_next_line.h"
+
+
+static void	ft_free(char **tofree)
+{
+	int	i;
+
+	if (!tofree)
+		return ;
+	i = 0;
+	while (tofree[i])
+	{
+		free((void *)tofree[i]);
+		i++;
+	}
+	free((void *)tofree);
+	return ;
+}
+
+static size_t	ft_str_len_count(char *str, char del, int mode)
+{
+	size_t	a;
+	size_t	i;
+
+	if (mode == 1)
+	{
+		i = 0;
+		str++;
+		while (*str)
+		{
+			if (*str == del && *(str - 1) != del)
+				i++;
+			str++;
+		}
+		if (*str == '\0' && *(str - 1) != del)
+			i++;
+		return (i);
+	}
+	a = 0;
+	while (*str != del && *str != '\0')
+	{
+		a++;
+		str++;
+	}
+	return (a);
+}
+
+static char	*ft_strdup_split(char *arr, char *s, char c, int *k)
+{
+	int		i;
+	int		len;
+
+	len = ft_str_len_count(&s[*k], c, 2);
+	if (len > 0)
+	{
+		arr = malloc(sizeof(char) * (len + 1));
+		if (!arr)
+			return (s);
+	}
+	i = 0;
+	while (s[*k] != c && s[*k] != '\0')
+	{
+		arr[i++] = s[*k];
+		*k += 1;
+	}
+	if (i == 0)
+		arr = NULL;
+	else
+		arr[i] = '\0';
+	return (arr);
+}
+
+static int	ft_core(char **arr, int count, char *s, char c)
+{
+	int		i;
+	int		k;
+
+	k = 0;
+	i = 0;
+	while (count > 0)
+	{
+		if (s[k] == c && c != '\0')
+			k++;
+		else
+		{
+			arr[i] = ft_strdup_split(arr[i], (char *)s, c, &k);
+			if (arr[i] == s)
+			{
+				arr[i] = NULL;
+				return (10);
+			}
+			i++;
+			count--;
+		}
+	}
+	if (count == 0)
+		arr[i] = NULL;
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	count;
+	char	**arr;
+
+	if (*s == '\0')
+		count = 0;
+	else
+		count = ft_str_len_count((char *)s, c, 1);
+	arr = malloc(sizeof(char *) * (count + 1));
+	if (!arr)
+	{
+		free((void *) arr);
+		return (NULL);
+	}
+	if (ft_core(arr, count, (char *)s, c) > 0)
+	{
+		ft_free(arr);
+		return (NULL);
+	}
+	return (arr);
+}
 
 char	*ft_strdup(char *src)
 {
@@ -58,14 +179,76 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (&res[-len]);
 }
 
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == 0)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	int		i;
+	size_t	act_l;
+	size_t	s_len;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	if (s_len < start)
+		sub = malloc(sizeof(char));
+	else
+	{
+		act_l = ft_strlen(&s[start]);
+		if (s_len >= start && act_l < len)
+			sub = malloc(sizeof(char) * (act_l + 1));
+		else
+			sub = malloc(sizeof(char) * (len + 1));
+		if (!sub)
+			return (NULL);
+		while (act_l-- > 0 && len-- > 0)
+			sub[i++] = s[start++];
+	}
+	if (!sub)
+		return (NULL);
+	sub[i] = '\0';
+	return (sub);
+}
+
+
+
 char	*get_next_line(int fd)
 {
-	static char	**stbuff[1048576 + 1];
+	static char	*stbuff[1048576 + 1];
+	char		buffer[BUFFER_SIZE + 1];
+	int			bytes_read;
+	char		*delloc;
+
+	if (stbuff == NULL)
+		stbuff[1048576] = malloc(sizeof(char)); //to free
+	stbuff[1048576][0] = 0;
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	buffer[bytes_read] = 0;
+	stbuff[fd] = ft_strdup(buffer); // to free
+	*delloc = ft_strchr(stbuff[fd], '\n');
+	if (delloc == &stbuff[bytes_read])
 
 
 
 
 
+
+
+	free(stbuff[1038576]);
 }
 
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 19:32:04 by maweiss           #+#    #+#             */
-/*   Updated: 2024/02/06 15:20:02 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/02/07 13:27:30 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 # include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <string.h>
+# include <ctype.h>
+# include <limits.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+# endif
 
 #endif
